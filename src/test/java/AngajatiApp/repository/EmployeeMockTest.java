bdd35ced3c494cd654f7252889dfc1345d4c980f
Diana Mihalescu <dianamihalescu25@gmail.com>
2024-11-02 07:44:43 +0200

    Corectare cod

--- src/test/java/AngajatiApp/repository/EmployeeMockTest.java
+++ src/test/java/AngajatiApp/repository/EmployeeMockTest.java
@@ -8,11 +8,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeMockTest {
-//declar o variabila de tip repository
+    //declar o variabila de tip repository
     private EmployeeMock employeeRepository;
     private DidacticFunction newFunction;
     private EmployeeMock employeeMock;
     private Employee existingEmployee;
+
     @BeforeEach
     void setUp() {
         //LAB2
@@ -20,59 +21,67 @@ class EmployeeMockTest {
         //lab3
         employeeMock = new EmployeeMock();
         newFunction = DidacticFunction.CONFERENTIAR;
-        existingEmployee = new Employee("Larson", "Puscas", "1234567890876", DidacticFunction.TEACHER,  2500d);
+        existingEmployee = new Employee("Larson", "Puscas", "1234567890876", DidacticFunction.TEACHER, 2500d);
         employeeMock.addEmployee(existingEmployee);
     }
-//lab2
+
+    //lab2
     @Test
     void addEmployeeT1() {
         assertEquals(6, employeeRepository.getEmployeeList().size());
-        employeeRepository.addEmployee(new Employee("diana",	"mihalescu","3849503829384", DidacticFunction.ASSISTENT,0.0));
-        assertEquals(6,employeeRepository.getEmployeeList().size());
-    }
-    @Test
-    void addEmployeeT2(){
+        employeeRepository.addEmployee(new Employee("diana", "mihalescu", "3849503829384", DidacticFunction.ASSISTENT, 0.0));
         assertEquals(6, employeeRepository.getEmployeeList().size());
-        employeeRepository.addEmployee(new Employee("Moni",	"Badiu","7483923028474", DidacticFunction.TEACHER,-5.0));
-        assertEquals(6,employeeRepository.getEmployeeList().size());
     }
+
     @Test
-    void addEmployeeT3(){
+    void addEmployeeT2() {
         assertEquals(6, employeeRepository.getEmployeeList().size());
-        employeeRepository.addEmployee(new Employee("diana",	"mihalescu","3849503829384", DidacticFunction.ASSISTENT,439049.0
+        employeeRepository.addEmployee(new Employee("Moni", "Badiu", "7483923028474", DidacticFunction.TEACHER, -5.0));
+        assertEquals(6, employeeRepository.getEmployeeList().size());
+    }
+
+    @Test
+    void addEmployeeT3() {
+        assertEquals(6, employeeRepository.getEmployeeList().size());
+        employeeRepository.addEmployee(new Employee("diana", "mihalescu", "3849503829384", DidacticFunction.ASSISTENT, 439049.0
         ));
-        assertEquals(7,employeeRepository.getEmployeeList().size());
+        assertEquals(7, employeeRepository.getEmployeeList().size());
     }
+
     @Test
-    void addEmployeeT4(){
+    void addEmployeeT4() {
         assertEquals(6, employeeRepository.getEmployeeList().size());
-        employeeRepository.addEmployee(new Employee("diana",	"mihalescu","3849503829384", DidacticFunction.ASSISTENT,43848.0
+        employeeRepository.addEmployee(new Employee("diana", "mihalescu", "3849503829384", DidacticFunction.ASSISTENT, 43848.0
         ));
-        assertEquals(7,employeeRepository.getEmployeeList().size());
+        assertEquals(7, employeeRepository.getEmployeeList().size());
     }
+
     @Test
-    void addEmployeeT5(){
+    void addEmployeeT5() {
         assertEquals(6, employeeRepository.getEmployeeList().size());
-        employeeRepository.addEmployee(new Employee("diana",	"pop9","7483923028474", DidacticFunction.LECTURER,4884.0
+        employeeRepository.addEmployee(new Employee("diana", "pop9", "7483923028474", DidacticFunction.LECTURER, 4884.0
         ));
-        assertEquals(6,employeeRepository.getEmployeeList().size());
-    }
-    @Test
-    void addEmployeeT6(){
         assertEquals(6, employeeRepository.getEmployeeList().size());
-        employeeRepository.addEmployee(new Employee("Moni",	"37474","3749402749304", DidacticFunction.TEACHER,4994.0
-        ));
-        assertEquals(6,employeeRepository.getEmployeeList().size());
     }
-//lab3
+
     @Test
-    void TC1(){
-        employeeMock.modifyEmployeeFunction(null,  newFunction);
+    void addEmployeeT6() {
+        assertEquals(6, employeeRepository.getEmployeeList().size());
+        employeeRepository.addEmployee(new Employee("Moni", "37474", "3749402749304", DidacticFunction.TEACHER, 4994.0
+        ));
+        assertEquals(6, employeeRepository.getEmployeeList().size());
+    }
+
+    //lab3
+    @Test
+    void TC1() {
+        employeeMock.modifyEmployeeFunction(null, newFunction);
         assertEquals(DidacticFunction.TEACHER, existingEmployee.getFunction());
     }
+
     //am apelat functia cu un employee null si am comparat functia angajatului existent cu ce avea el inainte sa ne asiguram ca nu s a mod; nu vrem sa se modifice functia angajatului existent daca i trimit un parametru null
     @Test
-    void TC2(){
+    void TC2() {
         employeeMock.modifyEmployeeFunction(existingEmployee, newFunction);
         assertEquals(existingEmployee.getFunction(), newFunction);
     }
